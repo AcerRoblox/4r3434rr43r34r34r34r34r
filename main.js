aa60b97f29224c4230ce965700d9fa504b1b31f3
AcerRoblox <94796790+AcerRoblox@users.noreply.github.com>
2022-03-19 13:46:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function changecolor(){
+  document.getElementById("nameheading").style.color.="green";
+}
+function changecolorblue(){
+  document.getElementById("nameheading").style.color.="blue";
+}
